6876b2accb612d1114edb178c9e8b73715c1cc30
Gabriole <ismail.mouhtaram@gmail.com>
2024-10-15 11:16:18 +0000

    Python3

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+import time
+from importlib import import_module as using
+
+async def measure_runtime() -> float:
+start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
